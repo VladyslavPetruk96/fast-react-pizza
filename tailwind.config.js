5d2741091321d32629d50beef3188f57a5243f3f
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-03-18 00:30:57 +0200

    finish styling app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+    extend: {
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
+
+// @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Lato:wght@400;700&family=Montserrat:wght@300;400;700&family=Roboto:wght@400;500;700;900&display=swap');
