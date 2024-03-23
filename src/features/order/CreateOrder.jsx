3f2b8bccf431173463dcb7b9979b294c7f678778
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-03-23 04:50:10 +0200

    add cartSlice + add items to cart & delete Items from cart func

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { getUsername } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,8 +38,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
-  const username = useSelector((state) => state.user.username);
-
+  const username = useSelector(getUsername);
   const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
